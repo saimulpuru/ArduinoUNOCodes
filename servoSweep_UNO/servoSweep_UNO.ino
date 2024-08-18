7a6c2372eb7cc31ab1ff513d740a98557fa2958c
Sai Krishna Mulpuru <saimulpuru@gmail.com>
2024-08-18 10:56:48 +0530

    Add files via upload

---
+++ servoSweep_UNO/servoSweep_UNO.ino
@@ -0,0 +1,24 @@
+//Code to sweep servo from 0 to 90 and back
+#include <Servo.h>
+
+const int servoPin1 = 3;  // Servo signal pin connected to Pin 3 on Nano
+
+Servo myServo1;
+
+void setup() {
+  myServo1.attach(servoPin1);
+  
+}
+
+void loop() {
+  // Move the servo motor to the starting position
+  myServo1.write(0);
+
+  delay(1000);
+
+  // Move the servo motor to a different position
+  myServo1.write(180);
+  
+  delay(1000);
+
+}
